57ad2b0052d9f9fa08fb9982bd5167c6d0c32302
impalerbxl <vladimirsida@centrum.cz>
2025-06-02 16:52:06 +0200

    Update sensor.py

--- custom_components/egdczpowerdata/sensor.py
+++ custom_components/egdczpowerdata/sensor.py
@@ -119,6 +119,7 @@ class EGDPowerDataSensor(Entity):
                     token_data = await response.json()
                     return token_data.get('access_token')
                     _LOGGER.info(f"---{token_data}---")
+                    _LOGGER.info(f"---{token}---")
                 else:
                     _LOGGER.error("Error retrieving access token")
                     return None
